5e0202a7bf5fb16d2dbbf863e8224577b77cf49f
vrenjithkr <vrenjithkr@gmail.com>
2022-01-30 12:53:17 +0530

    Uploaded samples for RequestMapping

--- src/main/java/com/renji/bank/controller/PaymentController.java
+++ src/main/java/com/renji/bank/controller/PaymentController.java
@@ -1,10 +1,39 @@
 package com.renji.bank.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/payment")
+@RequestMapping("/api/v1/bank")
 public class PaymentController {
+
+	@RequestMapping(value = "createUser", method = RequestMethod.POST)
+	public ResponseEntity<String> createUser() {
+		return new ResponseEntity<>("New user is created", HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "depositAmount ", method = RequestMethod.PUT)
+	public ResponseEntity<String> depositAmount() {
+		return new ResponseEntity<>("Amount is deposited", HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "addReceiverAccount", method = RequestMethod.POST)
+	public ResponseEntity<String> addReceiverAccount() {
+		return new ResponseEntity<>("Added receiver account", HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "transferAmount  ", method = RequestMethod.POST)
+	public ResponseEntity<String> transferAmount () {
+		return new ResponseEntity<>("Amount transfered", HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "balanceCheck ", method = RequestMethod.GET)
+	public ResponseEntity<String> balanceCheck () {
+		return new ResponseEntity<>("", HttpStatus.OK);
+	}
+
 
 }
